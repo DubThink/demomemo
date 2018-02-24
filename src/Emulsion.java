f247e5c4093b9c62886265b6524c972efb581df7
Benjamin Welsh <benjaminwelsh2@gmail.com>
2018-02-24 00:09:51 -0500

    more quvbe

--- src/Emulsion.java
+++ src/Emulsion.java
@@ -1,12 +1,4 @@
-/**
- * Created by benja on 2/27/2017.
- */
-import bpw.Util;
 import processing.core.PApplet;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Emulsion extends PApplet{
 
     public void settings(){
@@ -16,6 +8,8 @@ public class Emulsion extends PApplet{
     float ax,ay,az;
     public void draw(){
         background(10,10,30);
+        //fill(10,10,30,10);
+        //rect(0,0,width,height);
         noFill();
         pushMatrix();
         translate(width/2,height/2);
@@ -23,14 +17,19 @@ public class Emulsion extends PApplet{
         stroke(255);
         box(200);
         if(frameCount%63==0){
-            ax=random(-1,1);
-            ay=random(-1,1);
-            az=random(-1,1);
+            ax=round(random(-1,1));
+            ay=round(random(-1,1));
+            az=round(random(-1,1));
         }
         pushMatrix();
-        rotate(0.005f*(frameCount%63),ax,ay,az);
+
         stroke(255,255-5*(frameCount%63));
-        box(200+frameCount%63);
+        for(int i=1;i<10;i++){
+            rotate((i/10f)*0.005f*(frameCount%63),ax,ay,az);
+            stroke(255,(i%10f)+(i/10f)*(255-5*(frameCount%63)));
+            box(200+(i/1f)*(frameCount%63));
+        }
+
         popMatrix();
         popMatrix();
     }
